8b6948a3436f998b6f89e58ebcb3b4a8ff0d033a
Mychael Contreras <miketw1212@hotmail.com>
2022-11-16 19:33:53 -0700

    updated about me section

---
+++ my-app/src/Components/About/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div id="about">
+        <h1 id="main-section-header">About me</h1>
+        <hr id="main-section-hr" />
+        <div id="about-wrapper">
+            <img
+            id="portrait"
+            src="https://i.imgur.com/z8VyWj3.jpg?1"
+            alt="Self photo Mychael" />
+        <div id="about-inner-wrapper">
+        <h1>Hi, I'm Mychael!</h1>
+        <p>
+            I am a Full Stack Web Developer from Phoenix, AZ. I have a passion for creating applications that are unique and serve a deliberate solution to the end user. Problems are a part of the world we live in and I provide solutions to those problems by always learning and pushing myself to improve. I am never afraid to try something new, instinctively try something that might work, and what is worth trying! Welcome to a little bit about me!
+            <br />
+            <br />
+            As an adaptive person, I have become a lifetime student in front-end and back-end languages.
+            <br />
+            <br />
+            HTML, CSS, JavaScript, MySQL, Sequelize, MongoDB, Mongoose, GraphQL, Axios, Heroku, Git, Node.js, React.js, Express.js, Bootstrap, Materialize, jQuery, Localhost/storage, RESTful API's, 3rd Party API's, Insomnia, Moment.js, Webpack. Also experienced in web development models such as Object Oriented Progamming (OOP), Model-View-Controller (MVC), Progressive Web Apps (PWA), Test Driven Development (TDD), Object Relational Mapping (ORM) and Mongo-Express-React-Node (MERN). 
+            <br />
+            <br />
+            Currently learning Python and AWS databases. Obtained my MBA from Eastern New Mexico University in 2013 while playing soccer. 
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
