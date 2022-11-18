47e8569d6f6afb61e7294f4c23581d293af13047
Mychael Contreras <miketw1212@hotmail.com>
2022-11-17 19:42:27 -0700

    added footer with icons of linked in and git

---
+++ my-app/src/Components/Footer/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div id="footer">
+            <hr />
+            <div id="footer-wrapper">
+                <a
+                href="https://github.com/MychaelC"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <img
+                    className="footer-icons"
+                    src="https://i.imgur.com/Dr5B5zu.png"
+                    alt="GitHub logo"
+                    />
+                    </a>
+                    <a
+                    href="https://www.linkedin.com/in/mychael-contreras-2ba465184/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <img
+                    className="footer-icons"
+                    src="https://imgpile.com/images/RVPzv8.png"
+                    alt="LinkedIn logo"
+                    />
+                    </a>
+                    </div>
+                    </div>
+    );
+};
+
+export default Footer;
