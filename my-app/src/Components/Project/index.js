446e87490e8d0a334604268ec268801bd35d025d
Mychael Contreras <miketw1212@hotmail.com>
2022-11-18 19:25:05 -0700

    updated project index

---
+++ my-app/src/Components/Project/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Project = ({
+  title,
+  stack,
+  image_url,
+  image_alt,
+  github_link,
+  deployed_link,
+  nameOfClass,
+}) => {
+  return (
+    <div id="project-outter-wrapper">
+      <div id="single-project">
+        <div id="project-details">
+          <h1 id="project-title">{title}</h1>
+          <p id="project-stack">{stack}</p>
+        </div>
+        <img
+          className={nameOfClass}
+          id="project-image"
+          src={image_url}
+          alt={image_alt}
+        />
+      </div>
+      <div id="project-bottom-section">
+        <div id="project-links">
+          <a target="_blank" rel="noreferrer noopener" href={github_link}>
+            GitHub Repo
+          </a>
+          <a target="_blank" rel="noreferrer noopener" href={deployed_link}>
+            Deployed Link
+          </a>
+        </div>
+        <div id="project-desc">
+          <p>{title}</p>
+          <p>{stack}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
